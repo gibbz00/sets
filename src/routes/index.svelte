95f11b874550316dda1dbb4e159566288f39134d
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-07-10 18:29:32 +0000

    shoelace setup edits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,7 @@
-<script>
-	import { onMount } from 'svelte';
-    import "@shoelace-style/shoelace/dist/themes/light.css";
-	onMount(async () => {
-		await import('@shoelace-style/shoelace/dist/components/button/button');
-	});
-</script>
+<svelte:head>
+    <!-- HACK -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.77/dist/themes/light.css" />
+    <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.77/dist/shoelace.js"></script>
+</svelte:head>
 
-<sl-button>Test</sl-button>
+<sl-button>test</sl-button>
