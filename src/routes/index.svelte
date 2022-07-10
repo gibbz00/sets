c8dcc53f9d736f2a2eb4ebda82f507944159b416
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-07-10 22:23:56 +0000

    Autocomplete funcitonality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,4 +4,31 @@
     <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.77/dist/shoelace.js"></script>
 </svelte:head>
 
-<sl-button>test</sl-button>
+<script>
+    import AutoComplete from "$lib/AutoComplete.svelte"
+
+    let weekdays=[
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+    ]
+
+    let exercises=[
+        "Bench press",
+        "Dips",
+        "Deadlift",
+        "Squat"
+    ]
+</script>
+
+<sl-select>
+    {#each weekdays as weekday }
+        <sl-menu-item value={weekday}>{weekday}</sl-menu-item>
+    {/each}
+</sl-select>
+
+<AutoComplete data={exercises} placeholder="Add exercise"/>
