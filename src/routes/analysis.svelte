a2db1dffd12c0e02e3b2f5824b1ea379ba6101c1
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-07-14 21:11:57 +0000

    added create group functionality

--- src/routes/analysis.svelte
+++ src/routes/analysis.svelte
@@ -8,12 +8,27 @@
     import  WeekNames from "$lib/WeekNames.svelte"
     import SummedSetsMatrix from "$lib/SummedSetsMatrix.svelte"
     import { groups, weekNames, selectedGroup } from "$lib/Stores"
+    import HiddenAutoCompleteSelector from "$lib/HiddenAutoCompleteSelector.svelte";
+    import { ThrowSet } from "$lib/utils/ThowSet";
+
+    function createGroup(groupName: string){
+        if($groups.has(groupName))  {
+            //TODO: show error modal and let user continue typing the group
+        }
+        else {
+           $groups = $groups.set(groupName, new ThrowSet())
+        }
+    }
+
 </script>
 
 <nav>
     {#each Array.from($groups.keys()) as groupName}
         <span style:text-decoration={$selectedGroup == groupName ? "underline" : ""} on:click={() => {$selectedGroup = groupName}}>{groupName}</span>
     {/each}
+
+    <HiddenAutoCompleteSelector placeholder="Enter group name" on:selected={(event) => createGroup(event.detail)}/>
+
     <a href="/">Set planner</a>
 </nav>
 
