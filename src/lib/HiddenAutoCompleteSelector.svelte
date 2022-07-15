d752319ae63474374d9f0487ad36c63e04e353ce
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-07-15 19:06:58 +0000

    hide toggle input functionality

--- src/lib/HiddenAutoCompleteSelector.svelte
+++ src/lib/HiddenAutoCompleteSelector.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
+    /*
+        TODO: fully descript component behaviour
+        Description:
+            * Pressing escape should make autocomplete hidden, also resets data entry
+            * Pressing outside also makes autocomplete hidden
+    */
     import AutoCompleteSelector from "$lib/AutoCompleteSelector.svelte"
     let hidden: boolean = true
 
     export let data: string[] = []
     export let placeholder = ""
+
+    function handleKey(event: KeyboardEvent) {
+        if (!hidden && event.key == "Escape") hidden = true;
+    }
+    
 </script>
 
+<svelte:window on:keydown={(event) => handleKey(event) } on:click={() => { if(!hidden) hidden = true }}/>
+
 {#if hidden}
-    <span on:click={() => hidden = !hidden}> + </span>
+    <!-- stopPropagation required, window eventlistener will otherwise close it immediatedly -->
+    <span on:click|stopPropagation={() => hidden = !hidden}> + </span>
 {:else}
-    <AutoCompleteSelector slot="second" {data} {placeholder} on:selected/>  
+    <span on:click|stopPropagation={() => {}}>
+        <AutoCompleteSelector slot="second" {data} {placeholder} on:selected/>  
+    </span>
 {/if}
