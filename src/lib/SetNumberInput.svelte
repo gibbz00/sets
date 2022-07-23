1a3590b7766c43714b4d07ed5207a7a56dce3404
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-07-23 17:55:04 +0000

    set number incrementor

---
+++ src/lib/SetNumberInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let set: number
+</script>
+
+<div class="group">
+    <button class="hidden group-hover:inline-block" on:click={() => set--}>-</button>
+    <input 
+        class="w-7 text-center invalid:text-rose-600" 
+        min="0" 
+        step="1" 
+        type="number" 
+        bind:value={set}
+    >
+    <button class="hidden group-hover:inline-block" on:click={() => set++}>+</button>
+</div>
