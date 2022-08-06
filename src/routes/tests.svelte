8bc65d401058f0cb7f73e0e73827dcc94970d633
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-08-06 16:12:31 +0000

    filter function separated to file

--- src/routes/tests.svelte
+++ src/routes/tests.svelte
@@ -2,6 +2,7 @@
 	import SelectableInput from '$lib/testSnippets/SelectableInput.svelte'
 	import Hidden from '$lib/testSnippets/Hidden.svelte'
 	import HiddenSelectableInput from '$lib/testSnippets/HiddenSelectableInput.svelte'
+	import { autocompleteFilter } from '$lib/testSnippets/autoCompleteFilter'
 
 	/*
         Playground for testing snippets in dev environment
@@ -9,30 +10,7 @@
     */
 
 	const dataStub = ['Banana', 'Bluebery', 'Apple', 'Citrusfruits']
-
-	function autocompleteFilter(textFieldValue: string, items: string[]): string[] {
-		/*
-            Description:
-                * Autoselect option
-                    * Case-sensitive whole pattern based on textFieldValue 
-                    * Match starts only at first index 
-                    * Empty input means that every element is a potential match
-        */
-
-		// Then we iterate over each letter of the input and pick only those match charachter-wise
-		let potentialMatches: string[] = items
-		for (let index = 0; index < textFieldValue.length; index++) {
-			potentialMatches = potentialMatches.filter((potentialMatch) => {
-				if (potentialMatch.charAt(index) == textFieldValue.charAt(index)) {
-					return potentialMatch
-				}
-			})
-		}
-		return potentialMatches
-	}
 </script>
-
-<!-- <HiddenAutoComplete data={dataStub} placeholderText="Enter fruit" /> -->
 
 <Hidden>
 	<button slot="placeholderContent">Hidden</button>
