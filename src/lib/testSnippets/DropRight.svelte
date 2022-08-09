8373576666e3d19631e1220e393d714ce045629d
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-08-09 13:21:42 +0000

    continued rewrite

--- src/lib/testSnippets/DropRight.svelte
+++ src/lib/testSnippets/DropRight.svelte
@@ -28,73 +28,49 @@
 	let dropRightWindow: HTMLDivElement
 	let dropRightContainer: HTMLDivElement
 
-	let willUpdate: Promise<true>
-	let doneUpdate: Promise<true>
-
-	beforeUpdate(() => {
-		if (opened) {
-			console.log('huh')
-			willUpdate = Promise.resolve(true)
-		}
-	})
-
-	afterUpdate(() => {
-		if (opened) {
-			console.log('bik')
-			doneUpdate = Promise.resolve(true)
-		}
-	})
-
-	async function checkOpenToggle(event: MouseEvent) {
+	function checkOpenToggle(event: MouseEvent) {
 		if (!opened && dropRightContainer.contains(event.target as Node)) {
 			opened = true
 			return
 		}
 
-		// BUG: Contains can only run after droprightWindow has been updated,
-		// it tried to without any timeout and throws a null error.
-
-		// FIX:
-		// Sets a max-limit for how long it will wait before closing
-		// A limit that is hopefully longer than the time it takes to update
-		// The limit would propably suffice; but this method allows the contains method to be runned earlier if possible
 		if (opened) {
-			willUpdate = new Promise(() => {})
-			doneUpdate = new Promise(() => {})
-
-			//what happens if dropRightWindow is target?
-			// parent chech is required since clicked item might have dissapeared
-			setTimeout(() => {
-				if (!dropRightWindow.contains(event.target as Node)) {
-					// This won't work since I don't know how many levels are removed
-					// Thinking about creating a snapchot on function start and comparing it in this callback
-					console.log(dropRightWindow.contains((event.target as Node).parentElement))
-					opened = false
-					console.log('rans')
-				}
-				return
-			}, 200)
-
-			await doneUpdate
-			console.log('bok')
-			if (!dropRightWindow.contains(event.target as Node)) opened = false
+			if (!dropRightWindow.contains(event.target as Node)) {
+				opened = false
+			}
 		}
 	}
+
+	let overDropRight: boolean = false
 </script>
 
-<svelte:window on:click={(event) => checkOpenToggle(event)} />
+<!-- Capture since component might have been removed further down the event stack -->
+<svelte:window on:click|capture={(event) => checkOpenToggle(event)} />
 
-<div bind:this={dropRightContainer} class="group flex relative max-w-max">
-	<slot name="placeholderContent" />
-	<Icon
-		type="arrowRight"
-		class={`
-                    fill-gray-400 w-8 h-8 group-hover:fill-black 
-                    transition-transform ${opened ? 'rotate-180 fill-black' : ''}
+<!-- hover outside dropRightWindow when opneded should fill icon black -->
+<div bind:this={dropRightContainer} class="flex relative max-w-max">
+	<div class="contents group">
+		<slot name="placeholderContent" />
+		<Icon
+			type="arrowRight"
+			class={`
+                    w-8 h-8 group-hover:fill-black 
+                    transition-transform 
+                    ${
+						opened
+							? `rotate-180 ${overDropRight ? 'fill-gray-400' : 'fill-black'}`
+							: 'fill-gray-400'
+					}
                 `}
-	/>
+		/>
+	</div>
 	{#if opened}
-		<div bind:this={dropRightWindow} class="absolute top-1/2 left-[95%] z-10">
+		<div
+			on:mouseenter={() => (overDropRight = true)}
+			on:mouseleave={() => (overDropRight = false)}
+			bind:this={dropRightWindow}
+			class="absolute top-1/2 left-[95%] z-10"
+		>
 			<slot name="dropRightWindow" />
 		</div>
 	{/if}
