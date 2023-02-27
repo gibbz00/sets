5ed639418427edf0900d46338cdedffe77369fcf
Gabriel Hansson <gabrielhansson00@gmail.com>
2023-02-27 22:14:42 +0100

    Updates

---
+++ _app/immutable/chunks/1-efbcb0a0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-9758c785.js";export{t as component};
---
+++ _app/immutable/chunks/2-cdfad9c5.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/analysis/_page.svelte-b9c522fd.js";export{t as component,r as universal};
